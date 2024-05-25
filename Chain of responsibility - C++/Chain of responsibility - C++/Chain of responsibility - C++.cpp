0a88a117db3a87a8cc4be0b3d309898e5a570d31
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-25 17:00:21 +0200

    Add files via upload

---
+++ Chain of responsibility - C++/Chain of responsibility - C++/Chain of responsibility - C++.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+
+__interface IWorker {
+public:
+	virtual IWorker* SetNextWorker(IWorker* worker) = 0;
+	virtual string Execute(string command) = 0;
+};
+
+class AbstractWorker : public IWorker {
+private:
+	IWorker* next_worker;
+public:
+	AbstractWorker() : next_worker(nullptr){}
+	IWorker* SetNextWorker(IWorker* worker) override {
+		next_worker = worker;
+		return worker;
+	}
+	string Execute(string command) override {
+		if (next_worker)
+			return next_worker->Execute(command);
+		return{};
+	}
+};
+
+class Designer : public AbstractWorker {
+public:
+	string Execute(string command) override {
+		if (command == "design a house")
+			return "The designer executes the command: " + command;
+		else
+			return AbstractWorker::Execute(command);
+	}
+
+};
+
+class Carpenters : public AbstractWorker {
+public:
+	string Execute(string command) override {
+		if (command == "brick")
+			return "The carpenter followed the command: " + command;
+		else
+			return AbstractWorker::Execute(command);
+	}
+};
+
+class FinishWorker : public AbstractWorker {
+public:
+	string Execute(string command) override {
+		if (command == "wallpaper")
+			return "The interior construction worker executed the command: " + command;
+		else
+			return AbstractWorker::Execute(command);
+	}
+};
+
+void GiveCommand(IWorker* worker, string command) {
+	string str = worker->Execute(command);
+	if (str.empty()) {
+		cout << command << "- no one knows how to do\n";
+	}
+	else {
+		cout << str << "\n";
+	}
+}
+
+int main() {
+	Designer* designer = new Designer();
+	Carpenters* carpenters = new Carpenters();
+	FinishWorker* finish = new FinishWorker();
+
+	designer->SetNextWorker(carpenters)->SetNextWorker(finish);
+	GiveCommand(designer, "design a house");
+	GiveCommand(designer, "brick");
+	GiveCommand(designer, "wallpaper");
+
+	GiveCommand(designer, "wire");
+}
